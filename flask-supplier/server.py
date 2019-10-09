88fa0ddee71ad05080eda277c2b0283ecc8eba5c
Luke Martin <lukechile@gmail.com>
2019-10-09 17:40:34 -0400

    Add PyMongo

--- flask-supplier/server.py
+++ flask-supplier/server.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
+
+
 
 server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(server)
+
+# PyMongo connects to the MongoDB server running on port 27017 on localhost,
+# to the database named myDatabase.
+# This database is exposed as the db attribute. (mongo.db)
 
 @server.route('/')
 def index():
     return "Welcome to supplier team!"
+
+@server.route('/suppliers/<supplierID>', methods=['GET']))
+def supplier(supplierID):
+    # Got to connect to the MongoDB server
+    return supllierID
 
 if __name__ == '__main__':
     server.run()
