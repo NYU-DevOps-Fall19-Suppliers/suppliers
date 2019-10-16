2e48786287d7a3bbf23c0f71ef8c96d56e21f862
greenson <yanguangsen@gmail.com>
2019-10-16 01:17:37 -0400

    Added pylint.conf and improved model.py (still need more improvement)

--- service/models.py
+++ service/models.py
@@ -1,7 +1,10 @@
+"""
+A model file that defines the data schema and database operations
+"""
+
 import logging
-from flask import Flask
-from mongoengine import Document, StringField, ListField, IntField, DateTimeField, connect
-from mongoengine.errors import DoesNotExist, InvalidQueryError, ValidationError
+from mongoengine import Document, StringField, ListField, IntField, connect
+from mongoengine.errors import ValidationError
 from mongoengine.queryset.visitor import Q
 
 class DataValidationError(Exception):
@@ -71,7 +74,7 @@ class Supplier(Document):
         cls.logger.info('Initializing database')
         cls.app = app
         # This is where we initialize mongoDB from the Flask app
-        db = connect('myDatabase')
+        connect('myDatabase')
         app.app_context().push()
 
     @classmethod
@@ -96,7 +99,7 @@ class Supplier(Document):
         cls.logger.info('Processing looking for name %s', supplier_name)
         try:
             res = cls.objects.get(supplierName=supplier_name)
-        except ValidationError as e:
+        except ValidationError:
             raise DataValidationError('Invalid supplier_name: failed to ' \
                                       'find a supplier with given supplier_name')
         return res
@@ -106,7 +109,7 @@ class Supplier(Document):
     def find(cls, supplier_id):
         """Retrieves a single supplier with a given id (supplierID) """
 
-        cls.logger.info('Getting supplier with id: {}'.format(supplier_id))
+        cls.logger.info('Getting supplier with id: %s'.format(supplier_id))
 
         try:
             res = cls.objects(id=supplier_id).first()
@@ -143,35 +146,3 @@ class Supplier(Document):
         except ValidationError:
             return None
         return res
-
-
-    @classmethod
-    def find_by_product(cls, product_id):
-        """Retrieves a list of supplier with a given product id """
-        cls.logger.info("Getting suppliers with product id: %s".format(product_id))
-        return cls.objects(productIdList__in=product_id)
-
-    @classmethod
-    def find_by_rating(cls, rating):
-        """Retrieves a list of supplier with a given rating score """
-        cls.logger.info("Getting suppliers with ratting score greater than: %d".format(rating))
-        return cls.objects(averageRating__gte=3)
-
-    @classmethod
-    def action_make_recommendation(cls, product_id):
-        """Retrieves a list of supplier with a given rating score and product id """
-        cls.logger.info("Getting suppliers with ratting score greater than: %s".format(product_id))
-        return cls.objects(Q(productIdList__in=product_id) & Q(averageRating__gte=3))
-
-
-
-
-"""
-Test case for evaluating if the database is properly connected
-Should be removed as the further development goes
-"""
-# sup1 = Supplier(supplierID=123, supplierName = 'Walmart', address='NYC', averageRating=5)
-# sup1.save()
-
-# res = Supplier.objects(supplierName='sup1').first()
-# print(res.address)
