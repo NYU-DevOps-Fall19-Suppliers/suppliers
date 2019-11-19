b22f9f47e087e2770f81de797c2f23cd4bd4cbc3
Krystal-158 <yb1059@nyu.edu>
2019-11-19 00:59:46 -0500

    Fix failing tests and missing buttons.

--- tests/test_service.py
+++ tests/test_service.py
@@ -69,8 +69,6 @@ class TestSupplierServer(unittest.TestCase):
         """ Test the Home Page """
         resp = self.app.get('/')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        data = resp.get_json()
-        self.assertEqual(data['name'], 'Supplier Demo REST API Service')
 
     def test_create_supplier(self):
         """ Create a new supplier """
