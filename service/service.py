65df65c5140cb56e3465d9100d134c2291d7a04c
Sile Yang <yangsile123@gmail.com>
2019-12-04 01:03:05 -0500

    swagger create+list

--- service/service.py
+++ service/service.py
@@ -35,7 +35,7 @@ authorizations = {
     'apikey': {
         'type': 'apiKey',
         'in': 'header',
-        'name': 'X-Api-Key'
+        'name': 'X-API-KEY'
     }
 }
 
@@ -57,7 +57,7 @@ api = Api(app,
 # id_fields = {}
 # id_fields['$oid'] = fields.String(readOnly=True)
 supplier_model = api.model('Supplier', {
-    'id': fields.String(title = 'supplierId',
+    'id': fields.String(title = 'id',
                          description='The unique id assigned internally by service'),
     'supplierName': fields.String(required=True,
                           description='The name of the Supplier'),
@@ -69,7 +69,7 @@ supplier_model = api.model('Supplier', {
                                 description='The average rating of the Supplier')
 })
 
-create_model = api.model('supplier', {
+create_model = api.model('Supplier', {
     'supplierName': fields.String(required=True,
                           description='The name of the Supplier'),
     'address': fields.String(required=False,
@@ -93,18 +93,13 @@ supplier_args.add_argument('rating', type=int, required=False, help='List Suppli
 @app.errorhandler(DataValidationError)
 def request_validation_error(error):
     """ Handles Value Errors from bad data """
-    return bad_request(error)
-
-
-@app.errorhandler(status.HTTP_400_BAD_REQUEST)
-def bad_request(error):
-    """ Handles bad reuests with 400_BAD_REQUEST """
     message = str(error)
-    app.logger.warning(message)
-    return jsonify(status=status.HTTP_400_BAD_REQUEST,
-                   error='Bad Request',
-                   message=message), status.HTTP_400_BAD_REQUEST
-
+    app.logger.error(message)
+    return {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'error': 'Bad Request',
+        'message': message
+    }, status.HTTP_400_BAD_REQUEST
 
 @app.errorhandler(status.HTTP_404_NOT_FOUND)
 def not_found(error):
@@ -115,33 +110,23 @@ def not_found(error):
                    error='Not Found',
                    message=message), status.HTTP_404_NOT_FOUND
 
-@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
-def method_not_supported(error):
-    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
-    message = str(error)
-    app.logger.warning(message)
-    return jsonify(status=status.HTTP_405_METHOD_NOT_ALLOWED,
-                   error='Method not Allowed',
-                   message=message), status.HTTP_405_METHOD_NOT_ALLOWED
+# @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
+# def method_not_supported(error):
+#     """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+#     message = str(error)
+#     app.logger.warning(message)
+#     return jsonify(status=status.HTTP_405_METHOD_NOT_ALLOWED,
+#                    error='Method not Allowed',
+#                    message=message), status.HTTP_405_METHOD_NOT_ALLOWED
 
-@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-def mediatype_not_supported(error):
-    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
-    message = str(error)
-    app.logger.warning(message)
-    return jsonify(status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-                   error='Unsupported media type',
-                   message=message), status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
-
-
-@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
-def internal_server_error(error):
-    """ Handles unexpected server error with 500_SERVER_ERROR """
-    message = str(error)
-    app.logger.error(message)
-    return jsonify(status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                   error='Internal Server Error',
-                   message=message), status.HTTP_500_INTERNAL_SERVER_ERROR
+# @app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+# def mediatype_not_supported(error):
+#     """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+#     message = str(error)
+#     app.logger.warning(message)
+#     return jsonify(status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+#                    error='Unsupported media type',
+#                    message=message), status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
 
 ######################################################################
 # Authorization Decorator
@@ -150,8 +135,8 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
-        if 'X-Api-Key' in request.headers:
-            token = request.headers['X-Api-Key']
+        if 'X-API-KEY' in request.headers:
+            token = request.headers['X-API-KEY']
 
         if app.config.get('API_KEY') and app.config['API_KEY'] == token:
             return f(*args, **kwargs)
@@ -188,7 +173,6 @@ class SupplierResource(Resource):
     PUT /suppliers/{id} - Update a Supplier with the id
     DELETE /suppliers/{id} -  Deletes a Supplier with the id
     """
-
     #------------------------------------------------------------------
     # RETRIEVE A SUPPLIER
     #------------------------------------------------------------------ 
@@ -211,26 +195,74 @@ class SupplierResource(Resource):
 ######################################################################
 #  PATH: /suppliers
 ######################################################################
+@api.route('/suppliers', strict_slashes = True)
+class SupplierCollection(Resource):
+    """ Handles all interactions with collections of Suppliers """
+    #------------------------------------------------------------------
+    # LIST ALL SUPPLIERS
+    #------------------------------------------------------------------
+    @api.doc('list_suppliers')
+    @api.expect(supplier_args, validate=True)
+    @api.marshal_list_with(supplier_model)
+    @api.response(400, 'Bad Request')
+    def get(self):
+        """ Returns all of the Suppliers """
+        app.logger.info('Request to list Suppliers...')
+        suppliers = []
+        args = supplier_args.parse_args()
+        if args['rating']:
+            app.logger.info('Filtering by rating: %d', args['rating'])
+            suppliers = Supplier.find_by_rating(args['rating'])
+            if len(suppliers) == 0:
+                return '', status.HTTP_400_BAD_REQUEST
+        elif args['averageRating']:
+            app.logger.info('Filtering by rating: %s', args['averageRating'])
+            suppliers = Supplier.find_by_equals_to_rating(args['averageRating'])
+            if len(suppliers) == 0:
+                return '', status.HTTP_400_BAD_REQUEST
+        else:
+            suppliers = Supplier.all()
 
-######################################################################
-# GET A SUPPLIER
-######################################################################
-
-@app.route('/suppliers/<string:supplierID>', methods=['GET'])
-def get_a_supplier(supplierID):
-    """Gets a single supplier
-    This endpoint will get a Supplier based on a given supplierID
-    """
-    supplier = Supplier.find(supplierID)
-
-    if supplier is not None:
-        return make_response(supplier.to_json(), status.HTTP_200_OK)
-    return not_found("Not Found")
+        app.logger.info('[%s] suppliers returned', len(suppliers))
+        results = [supplier for supplier in suppliers]
+        return results, status.HTTP_200_OK
+    
+    #------------------------------------------------------------------
+    # ADD A NEW SUPPLIER
+    #------------------------------------------------------------------
+    @api.doc('create_suppliers', security='apikey')
+    @api.expect(create_model)
+    @api.response(400, 'The posted data was not valid')
+    @api.response(201, 'Supplier created successfully')
+    @api.marshal_with(supplier_model, code=201)
+    # @token_required
+    def post(self):
+        """
+        Creates a supplier
+        This endpoint will create a supplier based the data in the body that is posted
+        """
+        app.logger.info('Request to Create a supplier')
+        check_content_type('application/json')
+        data = request.get_json()
+        if not isinstance(data,dict):
+            data = json.loads(data)
+        try:
+            data['supplierName']
+        except KeyError as error:
+            raise DataValidationError('Invalid supplier: missing ' + error.args[0])
+        except TypeError as error:
+            raise DataValidationError('Invalid supplier: body of request contained'
+                                    'bad or no data')
+        supplier = Supplier(**data)
+        supplier.save()
+        app.logger.debug('Payload = %s', api.payload)
+        app.logger.info('supplier with new id [%s] saved!', supplier.id)
+        location_url = api.url_for(SupplierResource, supplier_id=supplier.id, _external=True)
+        return supplier, status.HTTP_201_CREATED, {'Location': location_url}
 
 ######################################################################
 # DELETE A SUPPLIER
 ######################################################################
-
 
 @app.route('/suppliers/<string:supplierID>', methods=['DELETE'])
 def delete_a_supplier(supplierID):
@@ -239,75 +271,6 @@ def delete_a_supplier(supplierID):
     if supplier:
         supplier.delete()
     return make_response('DELETED', status.HTTP_204_NO_CONTENT)
-
-######################################################################
-# ADD A NEW SUPPLIER
-######################################################################
-
-
-@app.route('/suppliers', methods=['POST'])
-def create_suppliers():
-    """
-    Creates a Supplier
-    This endpoint will create a Supplier based the data in the body that is posted
-    """
-    app.logger.info('Request to create a supplier')
-    check_content_type('application/json')
-    data = request.get_json()
-    if not isinstance(data,dict):
-        data = json.loads(data)
-
-    try:
-        data['supplierName']
-    except KeyError as error:
-        raise DataValidationError('Invalid supplier: missing ' + error.args[0])
-    except TypeError as error:
-        raise DataValidationError('Invalid supplier: body of request contained'
-                                  'bad or no data')
-    supplier = Supplier(**data)
-    supplier.save()
-
-    #location_url = url_for('get_suppliers', supplierID=supplier.id, _external=True)
-    return make_response(supplier.to_json(), status.HTTP_201_CREATED,
-        {'Location': url_for('get_a_supplier', supplierID=supplier.id, _external=True)})
-
-
-@app.route('/')
-def index():
-    """index"""
-    return app.send_static_file('index.html')
-    # return make_response(
-    #     jsonify(
-    #         name='Supplier Demo REST API Service',
-    #         version='1.0',
-    #         paths=url_for(
-    #             'list_suppliers',
-    #             _external=True)),
-    #     status.HTTP_200_OK)
-
-
-@app.route('/suppliers', methods=['GET'])
-def list_suppliers():
-    """ Route to list all suppliers 
-    Args:
-        rating: returns all the suppliers with average rating higher than rating.
-        averageRating: returns all the suppliers with average rating equaling to averageRating. 
-    """
-    app.logger.info('Request for supplier list')
-    rating = request.args.get('rating')
-    averageRating = request.args.get('averageRating')
-    if rating:
-        suppliers = Supplier.find_by_rating(rating)
-        if(len(suppliers) == 0):
-            return bad_request("Bad Request")
-    elif averageRating:
-        suppliers = Supplier.find_by_equals_to_rating(averageRating)
-        if(len(suppliers) == 0):
-            return bad_request("Bad Request")
-    else:
-        suppliers = Supplier.all()
-    return make_response(suppliers.to_json(), status.HTTP_200_OK)
-
 
 @app.route('/suppliers/<string:productId>/recommend', methods=['GET'])
 def action_recommend_product(productId):
